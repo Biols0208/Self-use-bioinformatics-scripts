3c953db64617cc2de44f7354917ad0f50bfc4494
Biols <50230939+Biols0208@users.noreply.github.com>
2026-09-11 10:35:47 +0800

    Update step3_make_lastz_chains.sh

--- genome/TOGA2/step3_make_lastz_chains.sh
+++ genome/TOGA2/step3_make_lastz_chains.sh
@@ -2,9 +2,9 @@
 set +o posix
 set -eo pipefail
 
-export JAVA_HOME=/ldfsqd1/ST_OCEAN/USER/c-lishuo/01_soft/jdk-24.0.1
-export PATH="/ldfsqd1/ST_OCEAN/USER/c-lishuo/01_soft/jdk-24.0.1/bin/:$PATH"
-export NXF_SINGULARITY_CACHEDIR=/ldfsqd1/ST_OCEAN/USER/c-lishuo/09_test/GL_RNAseq/SINGULARITY_CACHEDIR
+export JAVA_HOME=/01_soft/jdk-24.0.1
+export PATH="/01_soft/jdk-24.0.1/bin/:$PATH"
+export NXF_SINGULARITY_CACHEDIR=/01_soft/SINGULARITY_CACHEDIR
 
 ref_file=$PWD/prepare_data/ref.fa
 ref_name=ref
@@ -12,7 +12,7 @@ qur_file=$PWD/prepare_data/qur.fa
 qur_name=qur
 outdir=make_lastz_chains_out
 
-/ldfsqd1/ST_OCEAN/USER/c-lishuo/09_test/GL_RNAseq/nextflow run /ldfsqd1/ST_OCEAN/USER/c-lishuo/01_soft/make_lastz_chains/main.nf -params-file /ldfsqd1/ST_OCEAN/USER/c-lishuo/01_soft/make_lastz_chains/params.json -profile singularity \
+nextflow run /01_soft/make_lastz_chains/main.nf -params-file /01_soft/make_lastz_chains/params.json -profile singularity \
     --reference_genome ${ref_file} \
     --reference_name ${ref_name} \
     --query_genome ${qur_file} \
