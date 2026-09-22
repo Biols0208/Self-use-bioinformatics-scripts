f282ffdd2f189059d46dd321f3bf6beb128dfa31
Biols <50230939+Biols0208@users.noreply.github.com>
2026-09-22 16:02:55 +0800

    Update use.sh

--- python/gene_convert/use.sh
+++ python/gene_convert/use.sh
@@ -1,1 +1,1 @@
-python gene_convert.py -d all2.gene.clean.csv -t 30 --top-n 1 --scorer wratio -o out.tsv check_gene.csv 70
+python gene_convert.py -d all2.gene.clean.csv -t 30 --top-n 1 --scorer wratio -o out.tsv check_gene.tsv 70
